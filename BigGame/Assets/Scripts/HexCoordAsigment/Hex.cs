695e26fc621a7c30e72c1f5e5e2d7f022e69154c
GeneralGreedous <87451047+GeneralGreedous@users.noreply.github.com>
2022-12-11 14:15:42 +0100

    Spawn Manager

---
+++ BigGame/Assets/Scripts/HexCoordAsigment/Hex.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// podkradniête z t¹d https://youtu.be/htZijEO7ZmE
+
+[SelectionBase]
+public class Hex : MonoBehaviour
+{
+    [SerializeField]   
+    private HexCoordinates hexCoordinates;
+
+    public Vector3Int HexCoords => hexCoordinates.GetHexCoords();
+
+    private void Awake()
+    {
+        hexCoordinates = GetComponent<HexCoordinates>();
+       
+    }
+    
+
+}
