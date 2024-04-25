08f17e40d8fc00b0a2139f9d06f5530197b32107
SZtamjan <tamjan001@gmail.com>
2024-04-25 20:32:56 +0200

    Corrected info that was displayed on UI (instead of gain it was upgrade cost), Added window that follows cursor, displayed upgrade cost on new window, added conditions for window when it can be displayed

--- Assets/Scripts/GameManager/Building/Upgrade/DisplayInfo/DetectHoover.cs
+++ Assets/Scripts/GameManager/Building/Upgrade/DisplayInfo/DetectHoover.cs
@@ -3,14 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class DetectHoover : MonoBehaviour
 {
     [SerializeField] private GameObject popUpWindow;
-    
+    [SerializeField] private Button upgradeBtn;
+
     public void ActivateWindow()
     {
-        popUpWindow.SetActive(true);
+        if (upgradeBtn.interactable) //if button is disabled, then dont show popup window
+        {
+            popUpWindow.SetActive(true);
+        }
     }
 
     public void DisableWindow()
--- Assets/Scripts/GameManager/Building/Upgrade/DisplayInfo/PopUpDisplayBuildingInfo.cs
+++ Assets/Scripts/GameManager/Building/Upgrade/DisplayInfo/PopUpDisplayBuildingInfo.cs
@@ -1,25 +1,45 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PopUpDisplayBuildingInfo : MonoBehaviour
 {
-    private Coroutine followCor;
+    [Header("Move window")]
+    [SerializeField] private Camera mainCam;
+    [SerializeField] private RectTransform objToMove;
+    [SerializeField] private RectTransform parent;
+
+    [Header("Stats")] 
+    [SerializeField] private DisplayBuildingInfo infoScript; 
+    [SerializeField] private TextMeshProUGUI gold;
+    [SerializeField] private TextMeshProUGUI food;
+    [SerializeField] private TextMeshProUGUI wood;
+    [SerializeField] private TextMeshProUGUI stone;
+    private void Update()
+    {
+        MoveObjectWithBuildingInfo();
+    }
+
+    private void MoveObjectWithBuildingInfo()
+    {
+        Vector3 pos = Input.mousePosition;
+        pos.z = parent.position.z;
+        objToMove.position = mainCam.ScreenToWorldPoint(pos);
+    }
+
     private void OnEnable()
     {
-        followCor = StartCoroutine(FollowCursor());
+        FillData(infoScript.SelectedBuilding.GetComponent<BuildingController>().UpgradeCost);
     }
 
-    private void OnDisable()
+    private void FillData(ResourcesStruct res)
     {
-        if(followCor != null) StopCoroutine(FollowCursor());
-    }
-
-
-    private IEnumerator FollowCursor()
-    {
-        
-        yield return null;
+        gold.text = res.Gold.ToString();
+        food.text = res.Food.ToString();
+        wood.text = res.Wood.ToString();
+        stone.text = res.Stone.ToString();
     }
 }
