15651f8ddfb24c5f7b9dc06b960380b5b1bcdf10
GeneralGreedous <87451047+GeneralGreedous@users.noreply.github.com>
2024-06-19 20:38:02 +0200

    Staty do kart zwrot

--- Assets/Scripts/ScriptableObjects Scripts/Cards/SpellScriptableObjects.cs
+++ Assets/Scripts/ScriptableObjects Scripts/Cards/SpellScriptableObjects.cs
@@ -4,26 +4,32 @@ using UnityEngine;
 using UnityEngine.UI;
 using static SpellsStatsClass;
 
-
 [CreateAssetMenu(fileName = "SpellCard", menuName = "Cards/SpellCard")]
 public class SpellScriptableObjects : CardScriptableObject
 {
     [Header("Statystyki ")]
-    [SerializeField] int power;
-    [SerializeField] bool playerSide = true;
-    [SerializeField]SpellsScrptableObject spellsScrptableObject;
+    [SerializeField] private int power;
+
+    [SerializeField] private bool playerSide = true;
+    [SerializeField] private SpellsScrptableObject spellsScrptableObject;
+
     public override void CardAction(GameObject karta, CardScriptableObject stats)
-    {        
+    {
         SpellStats stat = (SpellStats)stats.GetStats();
         spellsScrptableObject.SpellAction(karta, stat.playersSide, stat.power);
+    }
+
+    public override int[] GetStatsCard()
+    {
+        //int[] staty = { power, 0 };
+        return new int[] { power, -1 };
     }
 
     public override object GetStats()
     {
         SpellStats stats = new SpellStats();
-        stats.power=power;
-        stats.playersSide=playerSide;
+        stats.power = power;
+        stats.playersSide = playerSide;
         return stats;
-
     }
 }
